325a5532c7e5d1e07984da1975ab1896bbdee7ff
JohnKaick <john.kaick@gmail.com>
2018-11-21 15:59:35 -0200

    publish website umbler

--- src/grupo/api.js
+++ src/grupo/api.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
 
 export function getOne(id) {
-    return axios.get(`http://apibet.jkapp.com.br/api/grupo/obter/${id}`)
+    return axios.get(`http://betapi.jkapp.com.br/api/grupo/obter/${id}`)
 }
 
 export function getAll() {
-    return axios.get(`http://apibet.jkapp.com.br/api/grupo`)
+    return axios.get(`http://betapi.jkapp.com.br/api/grupo`)
 }
 
 export function create(data) {
-    return axios.put(`http://apibet.jkapp.com.br/api/grupo/cadastrar`, {
+    return axios.put(`http://betapi.jkapp.com.br/api/grupo/cadastrar`, {
         nome: data.nome,       
     })
 }
 
 export function update(data) {
-    return axios.post(`http://apibet.jkapp.com.br/api/grupo/editar/${data.id}`, {
+    return axios.post(`http://betapi.jkapp.com.br/api/grupo/editar/${data.id}`, {
         nome: data.nome,       
     })
 }
 
 export function remover(id) {
-    return axios.delete(`http://apibet.jkapp.com.br/api/grupo/remover/${id}`)
+    return axios.delete(`http://betapi.jkapp.com.br/api/grupo/remover/${id}`)
 }
